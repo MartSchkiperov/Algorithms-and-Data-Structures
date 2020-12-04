da6d3655aaa9a88e8009f2e954f3f3ca777373ae
Mart Schkiperov <maschk@ttu.ee>
2020-12-04 19:01:38 +0200

    HashStructuresSpeed

--- HW02/src/HashStructuresSpeed.java
+++ HW02/src/HashStructuresSpeed.java
@@ -9,12 +9,14 @@ public class HashStructuresSpeed {
         HashMap<SimpleEntry, List> hashMap = new HashMap<>();
         Map<SimpleEntry, List> map = new HashMap<>();
         HashSet<SimpleEntry> hashSet = new HashSet<>();
+        List<SimpleEntry> list = new ArrayList<>();
         SimpleEntry[][] simpleEntryArray = new SimpleEntry[1000][1000];
 
         List<SimpleEntry> simpleEntryList = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
             simpleEntryList.add(new SimpleEntry(i, i+1));
         }
+
 
         long startTime = System.currentTimeMillis();
         for (int i = 0; i < 1_000_000; i++) {
@@ -23,6 +25,7 @@ public class HashStructuresSpeed {
         String numberString = numberToString((System.currentTimeMillis() - startTime));
         System.out.println("\nHashMap<SimpleEntry, List> million entries: " + numberString + "ms");
 
+
         startTime = System.currentTimeMillis();
         for (int i = 0; i < 1_000_000; i++) {
             map.put(new SimpleEntry(rand.nextInt(1000), rand.nextInt(1000)), simpleEntryList);
@@ -30,12 +33,29 @@ public class HashStructuresSpeed {
         numberString = numberToString((System.currentTimeMillis() - startTime));
         System.out.println("\nMap<SimpleEntry, List> million entries: " + (numberString + "ms"));
 
+
         startTime = System.currentTimeMillis();
         for (int i = 0; i < 1_000_000; i++) {
             hashSet.add(new SimpleEntry(rand.nextInt(1000), rand.nextInt(1000)));
         }
         numberString = numberToString((System.currentTimeMillis() - startTime));
         System.out.println("\nHashSet<SimpleEntry> million entries: " + numberString + "ms");
+
+        // WARNING!!! Takes a lot of time!
+        /*
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i < 1_000_000; i++) {
+            list.add(new SimpleEntry(1, i));
+        }
+        System.out.println("\n### Preparing List<SimpleEntry> million entries: " + (System.currentTimeMillis() - startTime) + "ms");
+
+        startTime = System.currentTimeMillis();
+        for (int i = 999_999; i > -1; i--) {
+            list.contains(new SimpleEntry(1, i));
+        }
+        numberString = numberToString((System.currentTimeMillis() - startTime));
+        System.out.println("\nList<SimpleEntry> contains million entries: " + numberString + "ms");
+        */
 
         startTime = System.currentTimeMillis();
         for (int i = 0; i < 1_000; i++) {
